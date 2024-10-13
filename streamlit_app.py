d037fc44d160998648c740f09ebc12e9b824667b
ankit dasuni <71224197+ankitdasuni@users.noreply.github.com>
2024-10-14 00:58:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 MENTAL_HEALTH')
 
 st.write('Hello world!')
+df = pd.read_csv("https://raw.githubusercontent.com/Mohsenmohebbi1993/penguins/refs/heads/main/penguins_size.csv")
+df
